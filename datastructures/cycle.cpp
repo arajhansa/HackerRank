afd10447eefaa2bdabdc1b7a45850d03eff8d254
arajhansa <rajhansa.advay@gmail.com>
2017-10-14 23:34:40 +0530

    commit 7: cycle created

--- datastructures/cycle.cpp
+++ datastructures/cycle.cpp
@@ -1,3 +1,19 @@
 bool has_cycle(Node *head){
-    
+    if(head==NULL){
+        return false;
+    }
+
+    Node slow = head;
+    Node fast = head;
+
+    while(!fast %% !fast.next){
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if(fast == slow){
+            return true;
+        }
+    }
+
+    return false;
 }
