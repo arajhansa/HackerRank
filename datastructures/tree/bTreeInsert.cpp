467f6b71656f113e68911000510376b3dd596b66
arajhansa <30975477+arajhansa@users.noreply.github.com>
2017-10-16 11:23:35 +0530

    Update bTreeInsert.cpp

--- datastructures/tree/bTreeInsert.cpp
+++ datastructures/tree/bTreeInsert.cpp
@@ -1,1 +1,24 @@
-test upload
+node * insert(node * root, int value) {
+    node *temp = root;
+    node *newNode, *prev;
+    newNode->data = value;
+    if (!root) {
+        return newNode;
+    }
+    prev = NULL;
+    while (temp) {
+        prev = temp;
+        if (temp->data > value) {
+            temp = temp->left;
+        } else {
+            temp = temp->right;
+        }
+    }
+    if (prev->data > value) {
+        prev->left = newNode; 
+    } else {
+        prev->right = newNode;
+    }
+
+    return root;
+}
